a3ac8fb0ffacfda15ddbc857465f478cd3ed1a27
Abdullah Numan <anewman15@hotmail.com>
2021-03-23 21:59:51 +0600

    Fix rubocop error

--- app/views/users/user_not_created.json.jbuilder
+++ app/views/users/user_not_created.json.jbuilder
@@ -1,2 +1,1 @@
 json.message 'User not created.'
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
+  root 'pages#index'
+  resources :users, defaults: { format: :json }
+  resource :users, only: [:update, :destroy]
 end
