9170a9b80d8bd4f1d148dadd105f8dd5837d9fac
Abdullah Numan <anewman15@hotmail.com>
2021-03-23 17:14:09 +0600

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { in: 5..20 }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
