92d85d6577cb9ce409f2fe986adad1f1773387a2
Abdullah Numan <anewman15@hotmail.com>
2021-03-23 18:53:34 +0600

    Remove validation conditionals from users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,6 @@ class UsersController < ApplicationController
     @user = User.new(create_user_params)
     if @user.save
       render :created, status: :ok
-    elsif User.exists?(username: @user.username) || User.exists?(email: @user.email)
-      render :user_exists, status: :unprocessable_entity
-    elsif @user.password != @user.password_confirmation
-      render :password_mismatch, status: :unprocessable_entity
     else
       render :user_not_created, status: :unprocessable_entity
     end
