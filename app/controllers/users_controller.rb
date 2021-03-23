d6a1bd17163d1b19c1625ee91d0237d6db2c84d3
Abdullah Numan <anewman15@hotmail.com>
2021-03-23 17:26:39 +0600

    Define users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    @user = User.new(create_user_params)
+    if @user.save
+      render :created, status: :ok
+    elsif User.exists?(username: @user.username) || User.exists?(email: @user.email)
+      render :user_exists, status: :unprocessable_entity
+    elsif @user.password != @user.password_confirmation
+      render :password_mismatch, status: :unprocessable_entity
+    else
+      render :user_not_created, status: :unprocessable_entity
+    end
+  end
 end
