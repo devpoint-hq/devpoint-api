632be6e50212123a18bec19a90f006b119aab8b3
Abdullah Numan <anewman15@hotmail.com>
2021-03-24 14:52:24 +0600

    Add validations to Appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,6 @@
 class Appointment < ApplicationRecord
+  validates :appointment_host_id, presence: true
+  validates :appointment_guest_id, presence: true
+  validates :time, presence: true
+  validates :online_link, presence: true
 end
