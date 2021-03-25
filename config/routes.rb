8e0b8e2be59bded07aa1e4cafd9c6e17c3d716c7
Abdullah Numan <anewman15@hotmail.com>
2021-03-25 16:05:19 +0600

    Nest :appointments routes to :users, define routes for :developers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, defaults: { format: :json } do
+    resources :appointments, defaults: { format: :json }
+  end
+
+  resources :developers, only: :index, defaults: { format: :json }
+
   root 'pages#index'
-  resources :users, defaults: { format: :json }
-  resource :users, only: [:update, :destroy]
 end
