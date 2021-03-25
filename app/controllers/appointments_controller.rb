f244e297379ef3fccbf0300aee5843965af313ee
Abdullah Numan <anewman15@hotmail.com>
2021-03-25 14:00:19 +0600

    Define appointments#create action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,19 @@
 class AppointmentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    return head(:unprocessable_entity) unless User.exists?(create_appointment_params[:appointment_host_id])
+
+    user = User.find(create_appointment_params[:appointment_host_id])
+    @appointment = user.hosted_appointments.build(create_appointment_params)
+    if @appointment.save
+      render :appointment_created, status: :created
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  def create_appointment_params
+    params.permit(:appointment_host_id, :appointment_guest_id, :time, :online_link)
+  end
 end
