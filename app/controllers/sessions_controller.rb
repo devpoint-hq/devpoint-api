f3e8f9f103b4a890af46dc6b0a38c497ccc8d573
Abdullah Numan <anewman15@hotmail.com>
2021-03-26 21:01:27 +0600

    Define sessions#create action with strong params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    return unless User.exists?(username: create_session_params[:username])
+
+    @user = User.find_by(username: create_session_params[:username])
+    if @user.valid_password?(create_session_params[:password])
+      render :session_created, status: :ok
+    else
+      render json: { message: 'Invalid credentails.' }
+    end
+  end
+
+  private
+
+  def create_session_params
+    params.permit(:username, :password)
+  end
 end
