278f6f282085a5ff08c131a9239c6604f1bc4574
Abdullah Numan <anewman15@hotmail.com>
2021-03-26 21:42:52 +0600

    Define sessions#destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,22 @@ class SessionsController < ApplicationController
 
     @user = User.find_by(username: create_session_params[:username])
     if @user.valid_password?(create_session_params[:password])
-      render :session_created, status: :ok
+      render :session_created, status: :created
     else
       render json: { message: 'Invalid credentails.' }
+    end
+  end
+
+  def destroy
+    @user = User.where(username: params[:username])
+    if @user.update!(
+      authentication_token: nil,
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+      render json: { message: 'Session ended successfully' }, status: :ok
+    else
+      head(:unauthorized)
     end
   end
 
