4188587924e543ccabc77d3c05ee8cfbaba92def
Abdullah Numan <anewman15@hotmail.com>
2021-03-29 23:29:46 +0600

    Add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, only: [:sessions], controllers: {sessions: 'users/sessions'}
+
   resources :users, defaults: { format: :json } do
     resources :appointments, defaults: { format: :json }
   end
 
-  get '/sessions', to: 'sessions#show'
-  put '/sessions', to: 'sessions#update'
+  get '/sessions/validate', to: 'users#validate_session'
 
   resources :developers, only: [:index, :show], defaults: { format: :json }
 
