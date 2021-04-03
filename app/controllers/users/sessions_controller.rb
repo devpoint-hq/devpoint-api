8f073db89d1424d6b935bcb4241f3bb0402e3be8
Abdullah Numan <anewman15@hotmail.com>
2021-04-03 18:52:58 +0600

    Modify json output, statuses in sessions_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -15,24 +15,26 @@ class Users::SessionsController < Devise::SessionsController
       sign_in(resource_name, resource)
       respond_with_authentication_token(resource)
     else
-      render json: { message: 'Invalid credentials' }
+      render json: { message: 'Invalid credentials' }, status: :unauthorized
     end
   end
 
   def destroy
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     yield if block_given?
-    render json: { message: 'Signed out successfully.' }
+    render json: { message: 'Signed out successfully.' }, status: :ok
   end
 
   protected
 
   def respond_with_authentication_token(resource)
     render json: {
-      success: true,
-      id: resource.id,
-      username: resource.username,
-      email: resource.email
-    }
+      message: 'New session created.',
+      user: {
+        id: resource.id,
+        username: resource.username,
+        email: resource.email
+      }
+    }, staus: :created
   end
 end
