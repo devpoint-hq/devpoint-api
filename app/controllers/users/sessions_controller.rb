5fb6b59b072b58ed161f572c58b19688d095e42f
Abdullah Numan <anewman15@hotmail.com>
2021-04-03 19:04:26 +0600

    Move session validation action from :sessons_controller to :users_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
 
-  def validate
-    if user_session
-      render json: { message: 'Your session is active.' }
-    else
-      render json: { message: 'Please sign in.' }
-    end
-  end
-
   def create
     if User.exists?(email: params[:user][:email])
       self.resource = warden.authenticate!(auth_options)
