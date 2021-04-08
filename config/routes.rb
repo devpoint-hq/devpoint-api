35e8d4926d30837b80fe362f0040b633f2648714
Abdullah Numan <anewman15@hotmail.com>
2021-04-09 01:02:34 +0600

    Unnest :appointments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, only: [:sessions], controllers: {sessions: 'users/sessions'}
 
-  resources :users, defaults: { format: :json } do
-    resources :appointments, defaults: { format: :json }
-  end
+  resources :users, defaults: { format: :json }
 
+  resources :appointments, defaults: { format: :json }
+  
   get '/sessions/validate', to: 'users#validate_session'
 
   resources :developers, only: [:index, :show], defaults: { format: :json }
