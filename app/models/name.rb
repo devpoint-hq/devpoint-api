115b6aad56241b1e5d8badf4eadc4ea46f179258
Abdullah Numan <anewman15@hotmail.com>
2021-04-09 01:04:28 +0600

    Remove duplicate validation for :last_name in Name model

--- app/models/name.rb
+++ app/models/name.rb
@@ -1,7 +1,6 @@
 class Name < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :last_name, presence: true
   validates :user_id, presence: true
 
   belongs_to :user, optional: true
