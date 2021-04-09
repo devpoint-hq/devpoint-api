69986c82f78446146154be72650bf2de903d8648
Abdullah Numan <anewman15@hotmail.com>
2021-04-09 22:55:22 +0600

    Add same_site: :none session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-DevPoint::Application.config.session_store :cookie_store, key: '_dev_point_session', secure: Rails.env.production?
+DevPoint::Application.config.session_store :cookie_store, key: '_dev_point_session', same_site: :none, secure: Rails.env.production?
