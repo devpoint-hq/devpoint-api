5465f641026fd18601d5f085fc861c61d13695bd
Abdullah Numan <anewman15@hotmail.com>
2021-04-09 19:51:09 +0600

    Remove https:// from host in config.hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,5 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.hosts << "https://anewman15-dev-point.herokuapp.com"
+  config.hosts << "anewman15-dev-point.herokuapp.com"
 end
