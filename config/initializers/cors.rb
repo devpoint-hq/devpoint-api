86ff7fe28fe5e910f303491c853b8f34512c9d96
Abdullah Numan <anewman15@hotmail.com>
2021-04-09 15:03:05 +0600

    Add origin and host to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins ['localhost:3001', 'https://anewman15-dev-point.netlify.app']
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
   end
 end
 
-Rails.application.config.hosts << "product.com"
+Rails.application.config.hosts << "https://anewman15-dev-point.herokuapp.com/"
