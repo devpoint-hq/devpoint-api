686004e6cae6b4601b4c51cdac708aae03fd8524
Abdullah Numan <anewman15@hotmail.com>
2021-04-11 15:45:18 +0600

    Create factory for users, write tests for user validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.unique.email }
+    username { Faker::Internet.unique.username }
+    password { Faker::Internet.password }
+    password_confirmation { password }
+    admin { false }
+    hiring_manager { true }
+    developer { false }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { FactoryBot.build(:user) }
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+  end
 end
