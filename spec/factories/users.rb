caa058eaf6fb8c4963bd9cad32b7b825417182ff
Abdullah Numan <anewman15@hotmail.com>
2021-04-11 21:23:29 +0600

    Write tests for developers_controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
     username { Faker::Internet.unique.username(specifier: 5..20) }
-    password { Faker::Internet.password(min_length: 10, max_length: 20) }
-    password_confirmation { password }
+    password { '12345678' }
+    password_confirmation { '12345678' }
     admin { false }
-    hiring_manager { true }
-    developer { false }
+    hiring_manager { false }
+    developer { true }
   end
 end
