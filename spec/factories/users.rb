c0ec5bec1cabcb9051e34940930c1b0e0a447e8d
Abdullah Numan <anewman15@hotmail.com>
2021-04-11 18:19:30 +0600

    Write associations tests for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.unique.email }
-    username { Faker::Internet.unique.username }
-    password { Faker::Internet.password }
+    username { Faker::Internet.unique.username(specifier: 5..20) }
+    password { Faker::Internet.password(min_length: 10, max_length: 20) }
     password_confirmation { password }
     admin { false }
     hiring_manager { true }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,18 @@ RSpec.describe User, type: :model do
     subject { FactoryBot.build(:user) }
 
     it { should validate_presence_of(:email) }
+    # it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:username) }
+    # it { should validate_uniqueness_of(:username) }
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:password_confirmation) }
+  end
+
+  describe 'assocations' do
+    it { should have_many(:names) }
+    it { should have_many(:links) }
+    it { should have_many(:skills) }
+    it { should have_many(:jobs) }
+    it { should have_many(:appointments) }
   end
 end
