195934418c712f477ed1407501f4d0e69c93a6b5
Abdullah Numan <anewman15@hotmail.com>
2021-04-13 20:54:42 +0600

    Remove Name model, delete migration file for CreateNames

--- spec/factories/names.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :name do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    other_names { Faker::Name.last_name }
-    association :user
-  end
-end
