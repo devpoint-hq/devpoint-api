e662c2a93afc644174329d169d9cd4f1f769d447
Abdullah Numan <anewman15@hotmail.com>
2021-04-13 20:57:34 +0600

    Add experience, projects, rating columns in skills table

--- db/migrate/20210403133400_create_skills.rb
+++ db/migrate/20210403133400_create_skills.rb
@@ -1,7 +1,10 @@
 class CreateSkills < ActiveRecord::Migration[6.0]
   def change
     create_table :skills do |t|
-      t.string :name
+      t.string :name, null: false
+      t.integer :years_of_experience, null: false
+      t.integer :number_of_projects, null: false
+      t.integer :self_rating, null: false
       t.integer :user_id
       t.timestamps
     end
