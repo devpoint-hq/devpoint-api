7e9db0ff180fbf35d860b98681b975f9a6fab8b7
Abdullah Numan <anewman15@hotmail.com>
2021-04-14 14:04:27 +0600

    Change action in routes, move :session_validated view to authentication dir

--- app/views/authentication/session_validated.json.jbuilder
+++ app/views/authentication/session_validated.json.jbuilder
@@ -3,4 +3,7 @@ json.user do
   json.id current_user.id
   json.email current_user.email
   json.username current_user.username
+  json.first_name current_user.first_name
+  json.last_name current_user.last_name
+  json.other_names current_user.other_names
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :appointments, defaults: { format: :json }
 
-  get '/sessions/validate', to: 'users#validate_session'
+  get '/sessions/validate', to: 'authentication#index'
 
   resources :developers, only: [:index, :show], defaults: { format: :json }
 
