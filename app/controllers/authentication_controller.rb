49ce766c7778d93325411f4398f5fb534ee45e0a
Abdullah Numan <anewman15@hotmail.com>
2021-04-14 14:03:15 +0600

    Move validate_session from users_controller to authentication#index

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,9 @@
 class AuthenticationController < ApplicationController
+  def index
+    if user_session
+      render :session_validated, status: :ok
+    else
+      render json: { message: 'No active session' }, status: :unauthorized
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def validate_session
-    if user_session
-      render :session_validated, status: :ok
-    else
-      render json: { message: 'No active session' }, status: :unauthorized
-    end
-  end
-
   def create
     @user = User.new(create_user_params)
     if @user.save
