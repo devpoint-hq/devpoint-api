57fae099c0ab8b8f0097b9ce35aec6b1cb5a72ff
Abdullah Numan <anewman15@hotmail.com>
2021-04-22 14:04:06 +0600

    Remove unnecesary routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,5 @@ Rails.application.routes.draw do
 
   resources :appointments, defaults: { format: :json }
 
-  get '/sessions/validate', to: 'authentication#index'
-
   resources :developers, only: [:index, :show], defaults: { format: :json }
-
-  root to: redirect('https://anewman15-dev-point.netlify.app/')
 end
