2444bb573d57c7f2326159d9f7e6e58623e61998
Abdullah Numan <anewman15@hotmail.com>
2021-07-02 21:50:49 +0600

    Set variable name to profile_image_id

--- app/controllers/direct_upload_controller.rb
+++ app/controllers/direct_upload_controller.rb
@@ -19,8 +19,8 @@ class DirectUploadController < ApplicationController
 
   def create_blob(blob_args)
     blob = ActiveStorage::Blob.create_before_direct_upload!(blob_args.to_h.deep_symbolize_keys)
-    pdf_id = SecureRandom.uuid # the name of the file will just be a UUID
-    blob.update_attribute(:key, "uploads/#{pdf_id}") # will put it in the uploads folder
+    profile_image_id = SecureRandom.uuid # the name of the file will just be a UUID
+    blob.update_attribute(:key, "uploads/#{profile_image_id}") # will put it in the uploads folder
     blob
   end
 
