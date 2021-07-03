087cc69215e11c8a4703899e50d99f37414a9398
Abdullah Numan <anewman15@hotmail.com>
2021-07-03 20:16:00 +0600

    Define update action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,21 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(create_user_params)
     @user.hiring_manager = true
 
     if @user.save
       render :created, status: :ok
     else
       render :user_not_created, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if current_user.update!(user_params)
+      render json: { message: 'User updated successfully.' }, status: :ok
+    else
+      render json: { message: 'Something went wrong. User not updated.' }, status: :unprocessable_entity
     end
   end
 
