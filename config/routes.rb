916840035008d8ce0b4e6ada864c20a3fa6c56c6
Abdullah Numan <anewman15@hotmail.com>
2021-07-24 21:17:25 +0600

    Add :jobs resources to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, only: [:sessions], controllers: {sessions: 'users/sessions'}, defaults: { format: :json }
 
   resources :users, defaults: { format: :json }
+  resources :developers, only: [:index, :show], defaults: { format: :json }
+  resources :jobs, except: [ :new, :edit ] defaults: { format: :json }
 
   post '/presigned_url', to: 'direct_upload#create'
 
   resources :appointments, defaults: { format: :json }
 
-  resources :developers, only: [:index, :show], defaults: { format: :json }
 end
