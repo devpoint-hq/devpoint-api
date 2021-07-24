7824b24507a488cb3e17a2ee50933100179e5f23
Abdullah Numan <anewman15@hotmail.com>
2021-07-24 21:20:22 +0600

    Add :links resources to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   resources :users, defaults: { format: :json }
   resources :developers, only: [:index, :show], defaults: { format: :json }
-  resources :jobs, except: [ :new, :edit ] defaults: { format: :json }
+  resources :jobs, except: [ :new, :edit ], defaults: { format: :json }
+  resources :links, except: [ :new, :edit ], defaults: { format: :json }
 
   post '/presigned_url', to: 'direct_upload#create'
 
