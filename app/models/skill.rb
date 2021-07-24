5d0d96a7ece21aed57781dadadfd1c34c511f4c0
Abdullah Numan <anewman15@hotmail.com>
2021-07-24 21:11:28 +0600

    Add validations to Skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,8 @@
 class Skill < ApplicationRecord
   validates :name, presence: true
-  validates :user_id, presence: true
+  validates :years_of_experience, numericality: true
+  validates :number_of_projects, numericality: { only_integer: true }
+  validates :self_rating, numericality: true
 
   belongs_to :user
 end
