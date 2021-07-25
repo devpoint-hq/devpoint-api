db632c57749ca65fbcd745fec297f983256c3cba
Abdullah Numan <anewman15@hotmail.com>
2021-07-25 23:42:21 +0600

    Define links#create action method

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,14 @@
 class LinksController < ApplicationController
-  
+  def create
+    @link = current_user.links.build(link_params)
+
+    if @link.save
+      render :link_created, status: :created
+    else
+      render :link_not_created, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def link_params
