8bc4982fca9426c129a2454a2a6eb123d0450d70
Abdullah Numan <anewman15@hotmail.com>
2021-07-26 22:39:50 +0600

    Define skill_params private method

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,9 @@
 class SkillsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  private
+
+  def skill_params
+    params.require(:skill).permit(:name, :years_of_experience, :number_of_projects, :self_rating)
+  end
 end
