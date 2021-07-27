03385a5ee5ebda999bb747ac3de82ed953f88c91
Abdullah Numan <anewman15@hotmail.com>
2021-07-27 23:44:37 +0600

    Define employments#create action method

--- app/controllers/employments_controller.rb
+++ app/controllers/employments_controller.rb
@@ -1,4 +1,16 @@
 class EmploymentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
+
+  def create
+    @employment = current_user.employments.build(employment_params)
+
+    if @employment.save
+      render :employment_created, status: :created
+    else
+      render :employment_not_created, status: :unprocessable_entity
+    end
+  end
 
   private
 
