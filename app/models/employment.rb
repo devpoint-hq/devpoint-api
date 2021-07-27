416486d59cfa31e754caed4d55b9629995ca41dc
Abdullah Numan <anewman15@hotmail.com>
2021-07-27 23:28:04 +0600

    Add model field validations for Employment

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,2 +1,8 @@
 class Employment < ApplicationRecord
+  validates :title, presence: true
+  validates :company_name, presence: true
+  validates :start_date, presence: true, date: { before: :end_date }
+  validates :end_date, presence: true, date: { after: :start_date }
+
+  belongs_to :user
 end
