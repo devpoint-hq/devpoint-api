0d08caf0c66fd818e2087288c72ed7a92e7b6f83
Abdullah Numan <anewman15@hotmail.com>
2021-07-27 23:20:31 +0600

    Define :emloyments table, run migration

--- db/migrate/20210727164754_create_employments.rb
+++ db/migrate/20210727164754_create_employments.rb
@@ -1,6 +1,11 @@
 class CreateEmployments < ActiveRecord::Migration[6.0]
   def change
     create_table :employments do |t|
+      t.string :title, null: false
+      t.string :company_name
+      t.datetime :start_date, null: false
+      t.datetime :end_date, null: false
+      t.integer :user_id
 
       t.timestamps
     end
