da14588ebfe5cd93a77190211a21c262ab734012
Abdullah Numan <anewman15@hotmail.com>
2021-07-27 23:43:24 +0600

    Define employments_params private method

--- app/controllers/employments_controller.rb
+++ app/controllers/employments_controller.rb
@@ -1,2 +1,8 @@
 class EmploymentsController < ApplicationController
+
+  private
+
+  def employment_params
+    params.require(:employment).permit(:title, :company_name, :start_date, :end_date)
+  end
 end
