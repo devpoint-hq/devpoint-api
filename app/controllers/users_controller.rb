7d64cafd0266467312127401271311c642d563a1
Abdullah Numan <anewman15@hotmail.com>
2021-08-01 15:19:41 +0600

    Modify users#update action for :profile_image_updated view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,9 @@ class UsersController < ApplicationController
     params = user_params.except(:profile_image)
     profile_image_attached = current_user.profile_image.attach(profile_image) if profile_image
 
-    if profile_image_attached || current_user.update!(params)
+    if profile_image_attached
+      render :profile_image_updated, status: :ok
+    elsif current_user.update!(params)
       render :user_updated, status: :ok
     else
       render :user_not_updated, status: :unprocessable_entity
@@ -30,15 +32,3 @@ class UsersController < ApplicationController
     params.require(:user).permit(user_attributes)
   end
 end
-
-
-# {
-#   "direct_upload": {
-#       "url": "https://dev-point-anewman15.s3.ca-central-1.amazonaws.com/uploads/abac3e8e-2735-47c7-b280-3f20f6b3f0cb?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIASSIJJW7R62KH74F7%2F20210731%2Fca-central-1%2Fs3%2Faws4_request&X-Amz-Date=20210731T150541Z&X-Amz-Expires=600&X-Amz-SignedHeaders=content-length%3Bcontent-md5%3Bcontent-type%3Bhost&X-Amz-Signature=309380e85da01e768ee76d89ecabfcc7d11b46609586dbc14025a90bf2aa56e4",
-#       "headers": {
-#           "Content-Type": "image/png",
-#           "Content-MD5": "C+rgtr+Mompxe1wK3PBP8g=="
-#       }
-#   },
-#   "blob_signed_id": "eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBJQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--053aa29b88e01713350505046f2b1c5f4a859823"
-# }
