e1c1e5b99f771c8c48ede157c9062bf7973b24f1
Abdullah Numan <anewman15@hotmail.com>
2021-08-05 23:57:24 +0600

    Define user_mailer#confirm_account mailer action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+  def confirm_account(user)
+    @user = user
+    mail(
+      from: 'confirm_account@anewman15-dev-point.netlify.app',
+      to: @user.email,
+      subject: 'Confirm Your Account at anewman15-dev-point'
+    )
+  end
 end
