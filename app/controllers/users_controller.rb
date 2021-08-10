4867132bdb5010f9be1e569cba98fa7c7301f1ee
Abdullah Numan <anewman15@hotmail.com>
2021-08-10 16:56:09 +0600

    Remove confirm_account email delivery from users#create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
 
     if @user.save
       render :created, status: :ok
-      UserMailer.confirm_account(@user).deliver
     else
       render :user_not_created, status: :unprocessable_entity
     end
