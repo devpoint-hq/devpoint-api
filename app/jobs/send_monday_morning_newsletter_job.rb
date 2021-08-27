b028e862aad018b3e1ab8689dabe69795fa66db6
Abdullah Numan <anewman15@hotmail.com>
2021-08-27 22:58:51 +0600

    Send monday morning emails to hiring_managers in SendMondayMorningNewsletterJob

--- app/jobs/send_monday_morning_newsletter_job.rb
+++ app/jobs/send_monday_morning_newsletter_job.rb
@@ -2,6 +2,10 @@ class SendMondayMorningNewsletterJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    @users = User.where(hiring_manager: true)
+    @users.each do |user|
+      UserMailer.send_monday_morning_newsletter(user).deliver
+      sleep 2
+    end
   end
 end
