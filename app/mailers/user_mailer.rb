10441ad5f6c6bd8844848fd93f860709f71c07fa
Abdullah Numan <anewman15@hotmail.com>
2021-08-27 22:55:16 +0600

    Define send_monday_morning_newsletter mailer in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,13 @@ class UserMailer < ApplicationMailer
       subject: 'Your account at DevPoint has been confirmed'
     )
   end
+
+  def send_monday_morning_newsletter(user)
+    @user = user
+    mail(
+      from: 'accounts@anewman15-dev-point.netlify.app',
+      to: @user.email,
+      subject: 'Monday is for People With a Mission'
+    )
+  end
 end
