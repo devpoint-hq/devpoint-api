1efcbe7a545e8b085db7dd7c402aa7cd0510c8f1
Abdullah Numan <anewman15@hotmail.com>
2021-08-28 22:05:15 +0600

    Define columns :newsletters_subcriptions table, run migration

--- db/migrate/20210828155741_create_newsletters_subcriptions.rb
+++ db/migrate/20210828155741_create_newsletters_subcriptions.rb
@@ -1,7 +1,8 @@
 class CreateNewslettersSubcriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :newsletters_subcriptions do |t|
-
+      t.boolean :monday_morning_newsletter, default: true
+      t.boolean :featured_developer_newsletter, default: true
       t.timestamps
     end
   end
