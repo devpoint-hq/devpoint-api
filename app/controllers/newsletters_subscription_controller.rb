59f078c9f1fb291f64493d19fbc68fce739e73aa
Abdullah Numan <anewman15@hotmail.com>
2021-08-30 12:56:31 +0600

    Define NewslettersSubscription#update action method

--- app/controllers/newsletters_subscription_controller.rb
+++ app/controllers/newsletters_subscription_controller.rb
@@ -1,2 +1,10 @@
 class NewslettersSubscriptionController < ApplicationController
+  def update
+    @newsletters_subscription = NewslettersSubscription.where(params[:id]).first
+    if @newsletters_subscription.update!(newsletters_subscription_params)
+      render json: { status: 'updated', message: 'Subscription updated successfully!' }
+    else
+      render status: :unprocessable_entity
+    end
+  end
 end
