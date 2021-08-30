5e1f7eee34489868c2287523cbf9a0c6c814a512
Abdullah Numan <anewman15@hotmail.com>
2021-08-30 18:15:57 +0600

    Set action_mailer.raise_delivery_errors to false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   # ActionMailer config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
   # mailcatcher config
