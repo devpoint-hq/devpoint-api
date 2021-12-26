c2a490213e57b8d6d17f15cca9ef92f1d4c955f1
Abdullah Numan <anewman15@hotmail.com>
2021-12-26 13:17:52 +0600

    Add cloudinary as active_storage service

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module DevPoint
     # config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.active_storage.service = :cloudinary
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
